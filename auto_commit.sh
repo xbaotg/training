c3fe75f8bcfabf3fd6ba47a38eee5de86944fe90
tgbao2703 <tgbao2703@gmail.com>
2021-07-21 23:00:31 +0700

    updated

--- auto_commit.sh
+++ auto_commit.sh
@@ -1,2 +1,2 @@
 #!bin/bash
-cd ~/Documents/ && git add -A && git commit --allow-empty-message -m '' && git push origin master
+cd ~/Documents/ && git add -A && git commit -m 'updated' && git push origin master
